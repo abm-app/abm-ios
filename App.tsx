fcf81e3ee6919127ad4cea55959ed685117c158f
Abin Anto <abin98anto@gmail.com>
2026-06-16 11:45:22 +0530

    feat: integrate custom fonts and wrap app with QueryClientProvider

--- App.tsx
+++ App.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { useFonts } from 'expo-font';
 
 import ENV from '@/config/env';
 import RootNavigator from '@/navigation/RootNavigator';
+import queryClient from '@/api/queryClient';
 
 let AppEntry: React.ComponentType;
 
 if (ENV.STORYBOOK_ENABLED) {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
   AppEntry = require('./.rnstorybook').default;
 } else {
   AppEntry = RootNavigator;
 }
 
-export default AppEntry;
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'AncizarSerif-Regular': require('./assets/fonts/AncizarSerif-Regular.ttf'),
+    'AncizarSerif-Bold': require('./assets/fonts/AncizarSerif-Bold.ttf'),
+    'AncizarSerif-Italic': require('./assets/fonts/AncizarSerif-Italic.ttf'),
+    'AncizarSerif-BoldItalic': require('./assets/fonts/AncizarSerif-BoldItalic.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppEntry />
+    </QueryClientProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
