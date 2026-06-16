bdc40a069522336997514cb3abfed37f15f4f038
Abin Anto <abin98anto@gmail.com>
2026-06-16 13:16:59 +0530

    chore: font error in app.tsx fixed

--- App.tsx
+++ App.tsx
@@ -16,14 +16,14 @@ if (ENV.STORYBOOK_ENABLED) {
 }
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, fontError] = useFonts({
     'AncizarSerif-Regular': require('./assets/fonts/AncizarSerif-Regular.ttf'),
     'AncizarSerif-Bold': require('./assets/fonts/AncizarSerif-Bold.ttf'),
     'AncizarSerif-Italic': require('./assets/fonts/AncizarSerif-Italic.ttf'),
     'AncizarSerif-BoldItalic': require('./assets/fonts/AncizarSerif-BoldItalic.ttf'),
   });
 
-  if (!fontsLoaded) {
+  if (!fontsLoaded && !fontError) {
     return (
       <View style={styles.loadingContainer}>
         <ActivityIndicator size="large" />
